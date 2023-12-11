d3f620a1a73371c27c97623ab7d6da7dfb0071e3
RomainRullier <romainrullier@MacBook-Pro-de-Romain.local>
2023-12-11 15:16:42 +0100

    streamlit

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 import streamlit as st
+
+st.set_page_config(page_title="Streamlit", page_icon="🔫", layout="wide")
+
+st.title("BDM Scrapping")
+
+st.sidebar.title("Menu")
+
+pages = ["Accueil", "Recherche"]
+choice = st.sidebar.selectbox("Choix de la page", pages)
+
+if choice == "Accueil":
+    st.header("Accueil")
+
+if choice == "Recherche":
+    st.header("Recherche")
+    search = st.text_input("Rechercher un article")
+    submit = st.button("Valider")
