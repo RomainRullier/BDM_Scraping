3cf09b86362cbeb8caf39f0b75fe8a74cd1d9169
ArmandThomas <arthomas2878@gmail.com>
2023-12-11 14:52:27 +0100

    bs4 requirements

--- scrapping/functions.py
+++ scrapping/functions.py
@@ -1,7 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 def export_articles_bm(string_search):
     url = "https://www.blogdumoderateur.com/?s=%"%(string_search)
     response = requests.get(url)
+
 
 
